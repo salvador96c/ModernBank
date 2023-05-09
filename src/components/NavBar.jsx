c5c861d46fd343071bc7a67673cf3e242ccc2872
Salvador Cabral <salvadormhoc@gmail.com>
2023-05-09 12:29:55 +0100

    bank start

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import {useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const NavBar = () => {
+  return (
+    <div className='w-full flex py-6 justify-between items-center navbar'></div>
+  )
+}
+
+export default NavBar
